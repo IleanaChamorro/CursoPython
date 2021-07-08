9660aa8596494015f17d9c0fb3f72369aa6fdf9f
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-07-08 17:14:28 -0300

    Manejo de Strings

---
+++ Modulo1/manejocadena.py
@@ -0,0 +1,16 @@
+#Cadena (String)
+bandaFav = "Led Zeppelin"
+genero =  "Rock Band"
+
+#Concatenar cadenas 
+#print("migrupo favorito es:" + bandaFav + " " + genero)
+print("Mi banda favorita es:", bandaFav, genero)
+
+# Error al sumar
+numero = "1"
+numero2 = "3"
+print( numero + numero2 )
+
+numero = 1
+numero2 = 3
+print(numero + numero2)
