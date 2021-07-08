d4d846966df8e466d62d049f925c63d27b3fbaf8
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-07-08 17:19:56 -0300

    Tipos Boolean

---
+++ Modulo1/booleanos.py
@@ -0,0 +1,11 @@
+# Tipos bool (boolean)
+mivar = True
+print(mivar)
+
+mivar = 3 > 2 #True
+print(mivar)
+
+if mivar: 
+    print("El resultado es true") #El resultado fue verdadero
+else:
+    print("El resultado fue false")
