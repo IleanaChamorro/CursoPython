b55b15ec82d4993b990fc2f467374682de4540da
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-07-09 10:20:25 -0300

    Procesado y Conversion en la entrada de datos

---
+++ Modulo1/input.py
@@ -0,0 +1,10 @@
+#Función input para procesar la entrada del usuario
+resultado = input("Escribe un mensaje: ")
+print("valor proporcionado: ", resultado)
+print("Fin código")
+
+#Conversion De Entrada de Datos
+numero1 = int(input("Escribe el primer número: "))
+numero2 = int(input("Escribe el segundo número: "))
+resultado = numero1 + numero2
+print("El resultado de la suma es: ", resultado)
