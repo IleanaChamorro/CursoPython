fa145010ece481a5c552dd4469bea0fb9b37a42b
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-07-09 10:28:57 -0300

    Ejercicio 1 y 2

---
+++ Modulo1/Ejercicio1.py
@@ -0,0 +1,3 @@
+#Califica tu día del 1 al 10
+calificacion = int(input("Como estuvo tu dia (1 al 10): "))
+print("Mi día estuvo de: ", calificacion)
---
+++ Modulo1/Ejercicio2.py
@@ -0,0 +1,5 @@
+#Se solicita incluir la siguiente información acerca de un libro: Titulo, Libro
+titulo = input("Proporciona el titulo: ")
+autor = input("Proporciona el autor: ")
+
+print(titulo, "fue escrito por:", autor)
