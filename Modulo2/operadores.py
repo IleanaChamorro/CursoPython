120d54052567d13f18eefabad692865d2b15432d
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-07-10 20:17:49 -0300

    Operadores aritmeticos

---
+++ Modulo2/operadores.py
@@ -0,0 +1,25 @@
+#Operadores
+a = 3
+b = 2
+
+
+resultado = a + b
+print(f'Resultado suma: {resultado}')
+
+resta = a - b
+print(f'Resultado es: {resta}')
+
+multiplicacion = a * b
+print(f'resultado multiplicacion: {multiplicacion}')
+
+division = a / b 
+print(f'resultado division: {division}')
+
+division = a // b
+print(f'resultado division(int): {division}')
+
+modulo = a % b
+print(f'resultado division(modulo): {modulo}')
+
+exponente = a ** b
+print(f'resultado exponente: {exponente}')
