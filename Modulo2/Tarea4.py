222d8773fb495ba314b928c118ff8ceaee5fc8d5
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-07-13 21:50:30 -0300

    Tarea 4, Ejercicio Tienda de Libros

---
+++ Modulo2/Tarea4.py
@@ -0,0 +1,8 @@
+#Consigna - Solicitar al usuario dos valores, y determinar cual número es el mayor
+numero1 = int(input("Proporciona el Número 1:"))
+numero2 = int(input("Proporciona el Número 2:"))
+
+if(numero1 > numero2):
+    print("Número 1 es mayor")
+else:
+    print("Número 2 es mayor") 
--- Modulo2/ejercicioTienda.py
+++ Modulo2/ejercicioTienda.py
@@ -1,14 +1,3 @@
-#Función input para procesar la entrada del usuario
-resultado = input("Escribe un mensaje: ")
-print("valor proporcionado: ", resultado)
-print("Fin código")
-
-#Conversion De Entrada de Datos
-numero1 = int(input("Escribe el primer número: "))
-numero2 = int(input("Escribe el segundo número: "))
-resultado = numero1 + numero2
-print("El resultado de la suma es: ", resultado)
-
 #Solicitar al usuario que escriba la informacion de cierto libro (nombre, id, precio, indicar si el envio es gratuito)
 print('Proporcione los siguiente datos del libro: ')
 nombre = input("Proporciona el nombre: ")
@@ -16,4 +5,14 @@ id = int(input("Proporciona el id: "))
 #precio = input("Proporciona el precio: ")
 precio = float(input("Proporciona el precio: "))
 envio = input("Indica si el envio es gratuito (True/false): ")
-  
+if envio == "True":
+        print("El envio es gratuito")
+if envio == "False":
+        print("El envio no es gratuito")
+
+print(f'''
+    Nombre: {nombre}
+    id: {id}
+    precio: {precio}
+    envio: {envio} 
+''')
