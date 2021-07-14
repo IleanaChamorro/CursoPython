81350f82401c7231f3b49effe9f1c1dd9fce06f1
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-07-14 09:39:00 -0300

    Agregado Carpetas - EjerciciosPractica y Operadores

--- Modulo1/input.py
+++ Modulo1/input.py
@@ -9,11 +9,3 @@ numero2 = int(input("Escribe el segundo número: "))
 resultado = numero1 + numero2
 print("El resultado de la suma es: ", resultado)
 
-#Solicitar al usuario que escriba la informacion de cierto libro (nombre, id, precio, indicar si el envio es gratuito)
-print('Proporcione los siguiente datos del libro: ')
-nombre = input("Proporciona el nombre: ")
-id = int(input("Proporciona el id: "))
-#precio = input("Proporciona el precio: ")
-precio = float(input("Proporciona el precio: "))
-envio = input("Indica si el envio es gratuito (True/false): ")
-  
