b5b67af6002e369d186a74732ab6f84769b82acb
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-07-15 13:36:47 -0300

    Ciclo For

---
+++ Modulo4/cicloFor.py
@@ -0,0 +1,8 @@
+#El bucle for se utiliza para recorrer los elementos de un objeto iterable (lista, tupla, conjunto, diccionario, …) y ejecutar un bloque de código. En cada paso de la iteración se tiene en cuenta a un único elemento del objeto iterable, sobre el cuál se pueden aplicar una serie de operaciones.
+
+cadena = 'Hola'
+
+for letra in cadena:
+    print(letra)
+else:
+    print('Fin ciclo For')
