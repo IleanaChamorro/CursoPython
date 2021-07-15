d87510fe5bd70ee848b0c3d2bb45794be482b1f3
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-07-15 13:27:02 -0300

    Ciclo While

---
+++ Modulo4/Ciclowhile.py
@@ -0,0 +1,17 @@
+#Un ciclo While es una estructura que se utiliza para ejecutar un bloque de código repetidamente hasta que se cumple una condición determinada.
+
+condicion = True
+
+#Loop infinito
+
+#while condicion:
+#    print('Ejecutando ciclo while')
+#else:
+#    print('Fin del ciclo while')
+
+contador = 0
+while contador < 3:
+    print(contador) # 0 , 1 , 2
+    contador += 1 #contador = contador + 1
+else:
+    print('Fin del ciclo while') #Fin del ciclo while
