c3d5bc22da156501d7c9881fce6416bb0d3334f8
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-07-21 21:34:04 -0300

    Mas de atributos de instancia y self

--- Modulo7/Clases y Objetos/objetos.py
+++ Modulo7/Clases y Objetos/objetos.py
@@ -12,7 +12,17 @@ class Persona:
     def __init__(self, nombre, apellido, edad):
         self.nombre = nombre
         self.apellido = apellido
-        self.edad = edad
+        self.edad = edad 
+
+        #Otra manera
+
+        # def __init__(this, nombre, apellido, edad):
+        #this.nombre = nombre
+        #this.apellido = apellido
+        #this.edad = edad
+  #Metodos de Instancia
+    def mostrar_detalles(self):
+      print(f'Persona: {self.nombre} ,{self.apellido}, {self.edad}')
 
 persona1 = Persona('Juan', 'Perez', 28)
 print(f'Objeto Persona1: {persona1.nombre}, {persona1.apellido}, {persona1.edad}')
@@ -29,3 +39,8 @@ print(f'Objeto Persona1: {persona1.nombre}, {persona1.apellido}, {persona1.edad}
 persona2 = Persona('Karla', 'Gomez', 30)
 print(f'Objeto Persona2: {persona2.nombre}, {persona2.apellido}, {persona1.edad}')
 #Objeto Persona2: Karla, Gomez, 30
+
+#Reutilizando metodo
+persona2.mostrar_detalles() #Persona: Karla ,Gomez, 30
+#Otra manera de mandar a llamar el metodo
+Persona.mostrar_detalles(persona1)
