d2628ee130f4795d02dc999edf8931910ef71ce4
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-08-03 18:30:22 -0300

    Herencia Multiple

---
+++ Modulo10/test_figura_geometrica.py
@@ -0,0 +1,6 @@
+from Cuadrado import Cuadrado
+cuadrado1 = Cuadrado(5, 'rojo')
+print(cuadrado1.ancho) #5
+print(cuadrado1.alto) #5
+print(cuadrado1.color) #rojo
+print(cuadrado1.calcular_area()) #25
