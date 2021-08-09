e115fb72658e49e5e603707b7a250fc700adfe94
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-08-09 16:53:48 -0300

    Ejercicio Figura Geometrica

--- Modulo10/test_figura_geometrica.py
+++ Modulo10/test_figura_geometrica.py
@@ -1,6 +1,13 @@
 from Cuadrado import Cuadrado
+from Rectangulo import Rectangulo
 cuadrado1 = Cuadrado(5, 'rojo')
 print(cuadrado1.ancho) #5
 print(cuadrado1.alto) #5
 print(cuadrado1.color) #rojo
 print(cuadrado1.calcular_area()) #25
+print(cuadrado1)
+#MRO - Method Resolution Order - nos permite saber la jerarquia de clases
+#print(Cuadrado.mro()) #[<class 'Cuadrado.Cuadrado'>, <class 'FiguraGeometrica.FiguraGeometrica'>, <class 
+
+
+rectangulo1 = Rectangulo(3, 8, 'verde')
