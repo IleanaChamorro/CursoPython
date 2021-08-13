6e7f59456d9d3e2f9b69b4dce4bcf5ab927fc262
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-08-13 16:40:13 -0300

    Constantes

---
+++ Modulo11/constantes.py
@@ -0,0 +1,6 @@
+#En python no existe el concepto de constantes es solo una convencion
+
+MI_CONSTANTE = 'valor de mi constante'
+
+class Matematicas:
+    PI = 3.1416
---
+++ Modulo11/test_constantes.py
@@ -0,0 +1,9 @@
+from constantes import *
+
+print(MI_CONSTANTE) #valor de mi constante
+print(Matematicas.PI)
+
+#En python se puede modificar el valor de las variables siempre
+#No se debe cambiar el valor de una constante
+MI_CONSTANTE = 'Nuevo valor'
+print(MI_CONSTANTE) #Nuevo valor
