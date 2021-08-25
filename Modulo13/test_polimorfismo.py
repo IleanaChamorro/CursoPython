cb66bf86e5f85a790493528a5d161532d6714539
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-08-24 23:44:56 -0300

    Polimorfismo

--- Modulo13/test_polimorfismo.py
+++ Modulo13/test_polimorfismo.py
@@ -5,6 +5,8 @@ def imprimir_detalles(objeto):
     print(objeto)
     #Indicar cual es el tipo de dato que estamos imprimiendo al momento de la ejecucion
     print(type(objeto))
+    if isinstance(objeto, Gerente): #Retorna valor boolean en caso de que el objeto sea instancia de la clase en caso contrario retorna falso
+        print(objeto.departamento)
 
 empleado = Empleado('Juan', 5000)
 imprimir_detalles(Empleado)
