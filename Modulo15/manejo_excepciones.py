3e442cc68907c4a83d09e76d936179fa996bf805
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-08-26 22:00:29 -0300

    Mas tipos de excepciones

--- Modulo15/manejo_excepciones.py
+++ Modulo15/manejo_excepciones.py
@@ -6,3 +6,31 @@ try:
     10/0
 except Exception as e:
     print(f'Ocurrio un error {e}') #Ocurrio un error division by zero
+
+
+resultado = None
+a = 10
+b = 0
+try:
+    resultado = a / b
+except Exception as e:
+    print(f'Ocurrio un error: {e}')
+
+print(f'Resultado: {resultado}')#Resultado: None
+print('Continuamos...')#Continuamos...
+
+#Otro tipo de excepcion
+resultado = None
+a = '10'
+b = 0
+try:
+    resultado = a / b
+except ZeroDivisionError as e:
+    print(f'Ocurrio un error: {e}, {type(e)}')
+except TypeError as e:
+    print(f'Ocurrio un error: {e}, {type(e)}')
+except Exception as e:
+    print(f'Ocurrio un error: {e}, {type(e)}')
+
+print(f'Resultado: {resultado}')#TypeError: unsupported operand type(s) for /: 'str' and 'int'
+print('Continuamos...')#Continuamos...
